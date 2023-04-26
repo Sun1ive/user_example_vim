010a180d0bb907e46990bb581f3835e8c6e56e89
Sunlive <sunliveua@gmail.com>
2023-04-26 12:02:29 +0300

    feat: added package-info plugin

--- plugins/user.lua
+++ plugins/user.lua
@@ -12,9 +12,13 @@ return {
   {
     "folke/todo-comments.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
-    config = function()
-      require("todo-comments").setup {}
-    end,
-    event = "VeryLazy"
-  }
+    config = function() require("todo-comments").setup {} end,
+    event = "VeryLazy",
+  },
+  {
+    "vuki656/package-info.nvim",
+    dependencies = { "MunifTanjim/nui.nvim" },
+    config = function() require("package-info").setup {} end,
+    event = "VeryLazy",
+  },
 }
