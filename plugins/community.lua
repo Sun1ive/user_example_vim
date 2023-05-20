acf652068205b717379e4426cd0cbe5056943f61
Sunlive <sunliveua@gmail.com>
2023-05-20 15:16:19 +0300

    feat: added indent plugin

--- plugins/community.lua
+++ plugins/community.lua
@@ -10,4 +10,17 @@ return {
   { import = "astrocommunity.colorscheme.tokyonight" },
   { import = "astrocommunity.colorscheme.gruvbox" },
   { import = "astrocommunity.colorscheme.everforest" },
+  -- { import = "astrocommunity.indent.indent-blankline-nvim" },
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    event = "User AstroFile",
+    opts = {
+      char = "│",
+      filetype_exclude = { "help", "alpha", "dashboard", "neo-tree", "Trouble", "lazy" },
+      show_trailing_blankline_indent = false,
+      show_current_context = false,
+      show_end_of_line = true,
+      char_list = { "⋅" },
+    },
+  },
 }
